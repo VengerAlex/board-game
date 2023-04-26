ca6dfedd70b134a1c60cd92d71a236b12d3edc1e
Olexander Venger <alexvenger90@gmail.com>
2023-04-26 23:38:19 +0300

    render board & add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import {useState} from "react";
+
+type Board = (number | null)[][]
+
+const INITIAL_BOARD = [[3, 2, 1], [null, 5, 4], [7, 8, 6]]
+
 export const App = () => {
+  const [board, setBoard] = useState<Board>(INITIAL_BOARD)
+
   return (
-      <h1>START</h1>
+      <div className='container'>
+        <div className="board">
+          {board.map(row => row.map(column => <button>{column}</button>))}
+        </div>
+      </div>
   )
 }
